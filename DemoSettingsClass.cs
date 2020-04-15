4780fdb15798512b1c39485dae77e92e5e5b92ae
Liam <mipen@windowslive.com>
2020-04-16 02:04:15 +1000

    Added a demo settings class for reference in the wiki

---
+++ DemoSettingsClass.cs
@@ -0,0 +1,34 @@
+using ModLib.Attributes;
+using System.Xml.Serialization;
+
+namespace MyMod
+{
+    public class MyModSettings : SettingsBase
+    {
+        public const string InstanceID = "MyModSettings";
+        public override string ModName => "MyMod";
+        public override string ModuleFolderName => MyModFolder;
+        public override string ID { get; set; } = InstanceID;
+
+        public static Settings Instance
+        {
+            get
+            {
+                return (MyModSettings)SettingsDatabase.GetSettings(InstanceID);
+            }
+        }
+
+        [XmlElement]
+        [SettingProperty("Some Multiplier", 1f, 3f, "This is a multiplier")]
+        [SettingPropertyGroup("Group 1")]
+        public float SomeMultiplier { get; set; } = 1.5f;
+        [XmlElement]
+        [SettingProperty("Some multiplier Enabled", "Enables SomeMultiplier")]
+        [SettingPropertyGroup("Group 1")]
+        public bool MultiplierEnabled { get; set; } = true;
+        [XmlElement]
+        [SettingProperty("Some Other Multiplier", 1f, 3f, "This is another multiplier")]
+        [SettingPropertyGroup("Group 2")]
+        public float SomeOtherMultiplier { get; set; } = 1.5f;
+    }
+}
